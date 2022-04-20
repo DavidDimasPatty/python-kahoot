2c310d416f7bd266a93085810bd6fe187e7474f8
unknown <daviddimas80@gmail.com>
2022-04-20 21:11:41 +0700

    bug fix server

--- client.py
+++ client.py
@@ -36,7 +36,7 @@ class Page(tk.Frame):
             send_length += b' ' * (self.HEADER - len(send_length))
             self.client.send(send_length)
             self.client.send(message)
-            print(self.client.recv(2048).decode(self.FORMAT)) 
+            return self.client.recv(2048).decode(self.FORMAT) 
         
     '''  def baca_file(self):
      file1 = open('quest.txt', 'r')
@@ -60,17 +60,32 @@ class Page(tk.Frame):
         button.pack(ipadx=10, ipady=10)
 
     def page2(self):
-        label=tk.Label(root, text = 'Pertanyaan')
-        button1=tk.Button(root, text = 'A', command = self.changepage)
-        button2=tk.Button(root, text = 'B', command = self.changepage)
-        button3=tk.Button(root, text = 'C', command = self.changepage)        
-        button4=tk.Button(root, text = 'D', command = self.changepage)
+        Lines=self.send("soal")
+        print(type(Lines))
+        print(len(Lines))
+        for i in range (0,len(Lines),6):
+            self.pertanyaan.append(Lines[i].strip())
+            self.opsi.append(Lines[i+1].strip())
+            self.opsi.append(Lines[i+2].strip())
+            self.opsi.append(Lines[i+3].strip())
+            self.opsi.append(Lines[i+4].strip())
+            self.jawaban.append(Lines[i+5].strip())
+        label=tk.Label(root, text = self.pertanyaan[self.quest])
+        button1=tk.Button(root, text = self.opsi[self.opsinum], command = self.changepage)
+        button2=tk.Button(root, text = self.opsi[self.opsinum+1], command = self.changepage)
+        button3=tk.Button(root, text = self.opsi[self.opsinum+2], command = self.changepage)        
+        button4=tk.Button(root, text = self.opsi[self.opsinum+3], command = self.changepage)
         label.place(relx=0.5, rely=0.1, anchor="center")
         button1.place(relx=0.1, rely=0.2, anchor="nw")
         button2.place(relx=0.1, rely=0.3, anchor="nw")
         button3.place(relx=0.8, rely=0.2, anchor="ne")
         button4.place(relx=0.8, rely=0.3, anchor="ne")
-
+        self.quest=self.quest+1
+        self.opsinum=self.opsinum+3
+        self.pagenum=self.pagenum+1
+        if(self.pagenum==10):
+            self.pagenum=0
+            
     def changepage(self):
         for widget in root.winfo_children():
             widget.destroy()
--- server.py
+++ server.py
@@ -9,6 +9,7 @@ class Server(tk.Frame):
         tk.Frame.__init__(self, *args, **kwargs)
         self.pagenum=0
         self.quest=0
+        self.opsinum=0
         self.HEADER = 64
         self.PORT = 5050
         self.SERVER = socket.gethostbyname(socket.gethostname())
@@ -48,6 +49,7 @@ class Server(tk.Frame):
          self.opsi.append(Lines[i+3].strip())
          self.opsi.append(Lines[i+4].strip())
          self.jawaban.append(Lines[i+5].strip())
+     return Lines
             
          
         
@@ -58,23 +60,27 @@ class Server(tk.Frame):
         button.pack(ipadx=10, ipady=10)
 
     def page2(self):
-        label=tk.Label(root, text = 'Pertanyaan')
-        button1=tk.Button(root, text = 'A', command = self.changepage)
-        button2=tk.Button(root, text = 'B', command = self.changepage)
-        button3=tk.Button(root, text = 'C', command = self.changepage)        
-        button4=tk.Button(root, text = 'D', command = self.changepage)
+        label=tk.Label(root, text = self.pertanyaan[self.quest])
+        button1=tk.Button(root, text = self.opsi[self.opsinum], command = self.changepage)
+        button2=tk.Button(root, text = self.opsi[self.opsinum+1], command = self.changepage)
+        button3=tk.Button(root, text = self.opsi[self.opsinum+2], command = self.changepage)        
+        button4=tk.Button(root, text = self.opsi[self.opsinum+3], command = self.changepage)
         label.place(relx=0.5, rely=0.1, anchor="center")
         button1.place(relx=0.1, rely=0.2, anchor="nw")
         button2.place(relx=0.1, rely=0.3, anchor="nw")
         button3.place(relx=0.8, rely=0.2, anchor="ne")
         button4.place(relx=0.8, rely=0.3, anchor="ne")
+        self.quest=self.quest+1
+        self.opsinum=self.opsinum+3
+        self.pagenum=self.pagenum+1
+        if(self.pagenum==10):
+            self.pagenum=0
 
     def changepage(self):
         for widget in root.winfo_children():
             widget.destroy()
-        if self.pagenum == 1:
+        if self.pagenum ==1 <=9 :
             self.page2()
-            self.pagenum = 0
         elif self.pagenum  == 0:
             self.page1()
             self.pagenum = 1
@@ -90,6 +96,11 @@ class Server(tk.Frame):
                 msg = conn.recv(msg_length).decode(self.FORMAT)
                 if msg== "fail":
                     conn.send("WRONG INPUT FORMAT! TRY AND CHECK AGAIN".encode(self.FORMAT))
+                if msg=="soal":
+                    soal=self.baca_file()
+                    y = str(soal)
+                    y = y.encode()
+                    conn.send(y)
                 else:
                     res=eval(msg)
                     print(res)
@@ -100,7 +111,7 @@ class Server(tk.Frame):
                     conn.send(bye.encode(self.FORMAT))
                     connected = False
 
-            print(f" Success Calculator From [{addr}]")
+            #print(f" Success Calculator From [{addr}]")
         conn.close()       
     
     def start(self):
