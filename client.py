4340cd248a34f1fdc2dafc4f70cc7f370595f84e
unknown <daviddimas80@gmail.com>
2022-04-21 18:24:51 +0700

    final

--- client.py
+++ client.py
@@ -10,6 +10,7 @@ class Page(tk.Frame):
         self.quest=0
         self.opsinum=0
         self.score=0
+        self.Lines=""
         #arrray pertanyaan
         self.pertanyaan=[]
         #arrray opsi
@@ -30,9 +31,9 @@ class Page(tk.Frame):
         self.DISCONNECT_MESSAGE = "!DISCONNECT"
         self.SERVER = socket.gethostbyname(socket.gethostname())
         self.ADDR = (self.SERVER, self.PORT)
-
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.client.connect(self.ADDR)
+        self.get_soal()
 
     def send(self,msg):
             message = msg.encode(self.FORMAT)
@@ -55,7 +56,13 @@ class Page(tk.Frame):
          self.opsi.append(Lines[i+3].strip())
          self.opsi.append(Lines[i+4].strip())
          self.jawaban.append(Lines[i+5].strip()) '''
-            
+    def page3(self):
+        label=tk.Label(root, text = 'Final Score of you:')
+        label2=tk.Label(root, text = str(self.score*10))
+        button=tk.Button(root, text = 'Go To Home Page', command = self.changepage)
+        label.pack(ipadx=10, ipady=10)
+        label2.pack(ipadx=10, ipady=10)
+        button.pack(ipadx=10, ipady=10)           
          
         
     def page1(self):
@@ -63,47 +70,56 @@ class Page(tk.Frame):
         button=tk.Button(root, text = 'Start', command = self.changepage)
         label.pack(ipadx=10, ipady=10)
         button.pack(ipadx=10, ipady=10)
-
+    
+    def get_soal(self):
+        self.Lines=self.send("soal")
+        self.Lines=literal_eval(self.Lines)
+        for i in range (0,len(self.Lines),6):
+            self.pertanyaan.append(self.Lines[i].strip())
+            self.opsi.append(self.Lines[i+1].strip())
+            self.opsi.append(self.Lines[i+2].strip())
+            self.opsi.append(self.Lines[i+3].strip())
+            self.opsi.append(self.Lines[i+4].strip())
+            self.jawaban.append(self.Lines[i+5].strip())
+             
     def page2(self):
-        Lines=self.send("soal")
-        Lines=literal_eval(Lines)
-        print(type(Lines))
-        print(len(Lines))
-        for i in range (0,len(Lines),6):
-            self.pertanyaan.append(Lines[i].strip())
-            self.opsi.append(Lines[i+1].strip())
-            self.opsi.append(Lines[i+2].strip())
-            self.opsi.append(Lines[i+3].strip())
-            self.opsi.append(Lines[i+4].strip())
-            self.jawaban.append(Lines[i+5].strip())
         label=tk.Label(root, text = self.pertanyaan[self.quest])
-        button1=tk.Button(root, text = self.opsi[self.opsinum], command = self.changepage)
-        button2=tk.Button(root, text = self.opsi[self.opsinum+1], command = self.changepage)
-        button3=tk.Button(root, text = self.opsi[self.opsinum+2], command = self.changepage)        
-        button4=tk.Button(root, text = self.opsi[self.opsinum+3], command = self.changepage)
+        button1=tk.Button(root, text = self.opsi[self.opsinum], command =lambda:[self.checkjawab(self.opsi[self.opsinum-4],self.jawaban[self.quest-1]), self.changepage()])
+        button2=tk.Button(root, text = self.opsi[self.opsinum+1], command = lambda:[self.checkjawab(self.opsi[self.opsinum+1-4],self.jawaban[self.quest-1]), self.changepage()])
+        button3=tk.Button(root, text = self.opsi[self.opsinum+2], command = lambda:[self.checkjawab(self.opsi[self.opsinum+2-4],self.jawaban[self.quest-1]), self.changepage()])
+        button4=tk.Button(root, text = self.opsi[self.opsinum+3], command = lambda:[self.checkjawab(self.opsi[self.opsinum+3-4],self.jawaban[self.quest-1]), self.changepage()])
         label.place(relx=0.5, rely=0.1, anchor="center")
         button1.place(relx=0.1, rely=0.2, anchor="nw")
         button2.place(relx=0.1, rely=0.3, anchor="nw")
         button3.place(relx=0.8, rely=0.2, anchor="ne")
         button4.place(relx=0.8, rely=0.3, anchor="ne")
-        
-            
+        self.quest=self.quest+1
+        self.opsinum=self.opsinum+4
+        self.pagenum=self.pagenum+1
+                 
      #check jawaban
-    def check_jawaban(self,jawaban,opsi):
-            if(jawaban==opsi):
-                self.score=+1
-            self.quest=self.quest+1
-            self.opsinum=self.opsinum+3
-            self.pagenum=self.pagenum+1  
+    def checkjawab(self,opsi,jawaban):
+         if opsi==jawaban:
+             self.score=self.score+1 
+         print(self.score)
+         print(jawaban)  
+         print(opsi)         
             
     def changepage(self):
-         for widget in root.winfo_children():
-                widget.destroy()
-         if self.pagenum >=1 and self.pagenum <=9 :
+        for widget in root.winfo_children():
+            widget.destroy()
+        if self.pagenum >=1 and self.pagenum <=10 :
             self.page2()
-         elif self.pagenum  == 0:
+        elif self.pagenum  == 0:
             self.page1()
             self.pagenum = 1
+        elif self.pagenum  == 11:
+            self.page3()
+            self.pagenum = 0    
+            self.quest=0
+            self.opsinum=0
+            self.score=0
+        #root.mainloop()
             
     
 
